142e07cfd623d8c51bc86c62be79a638cad6c21a
Darshana275 <spunna2705@gmail.com>
2025-06-21 22:38:48 +0530

    modified to deploy using netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/darshana-portfolio/", // ✅ Add this line
   server: {
     host: "::",
     port: 8080,
